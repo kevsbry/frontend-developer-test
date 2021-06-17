ef857c354ba3535df08c7e9c48789fd582e92983
kevsbry <nivekbryan09.projects@gmail.com>
2021-06-17 14:46:11 +0800

    install packages and make changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import "./App.css";
+import { BrowserRouter as Router, Switch } from "react-router-dom";
+import { HomePage, DevicesPage } from "./pages";
+import { PrivateRoute, PublicRoute } from "./components";
+import { Provider } from "react-redux";
+import { store } from "./global-states";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Provider store={store}>
+          <PublicRoute exact path="/" component={HomePage} />
+          <PrivateRoute exact path="/devices" component={DevicesPage} />
+        </Provider>
+      </Switch>
+    </Router>
   );
 }
 
